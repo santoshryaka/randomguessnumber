f4efabbff26b747c8730858acf002ebd242fa784
Santosh Ryaka <ryakasantosh@gmail.com>
2026-05-07 20:32:24 +0530

    Refactor number guessing game output and formatting

--- randomnumber.py
+++ randomnumber.py
@@ -1,19 +1,19 @@
 import random
 
 #Generates a random number
-secret= random.randint(1, 100)
-guess=0
-print("welcome to the number guessing game")
-print("guess the number between 1 to 100")
+secret = random.randint(1,100)
+guess = 0
+print("__welcome to the number guessing game__")
+print("guess the number between 1 to 100.")
 
 #loops run until correct guess
 while guess != secret:
     guess=int(input("enter your guess:"))
     if guess<secret:
-        print("too low! try again")
+        print("too low! try again.")
     elif guess>secret:
-        print("too high! try again")
+        print("too high! try again.")
     else:
         print("con*gratulations! you guessed the number.") 
         break
-print("Thankyou for playing this game")
+print("__Thankyou for playing this game__")
